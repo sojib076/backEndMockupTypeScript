89f88e74b173a8f3f0c0a5a80c881f27775f9047
sojib076 <sojibds332@gmail.com>
2024-05-21 17:31:32 +0600

    Commented

--- src/modules/order/order.Services.ts
+++ src/modules/order/order.Services.ts
@@ -3,24 +3,24 @@ import { orderModel } from "./order.Model";
 import { Torder } from "./order.Type";
 
 const createOrder = async (order: Torder) => {
+  const orderID = order.productId; // get the product ID from the order
 
-  const orderID = order.productId;     // get the product ID from the order 
-
-  const quantity = order.quantity;   // get the quantity from the order
+  const quantity = order.quantity; // get the quantity from the order
 
   const product = await ProductModel.findOne({ _id: orderID }); // find the product by the ID
 
   if (!product) {
+    // this is work only if the product is not found and give the error message
     throw new Error("Please provide a valid product ID");
   } else if (product && product.inventory.quantity >= quantity) {
+    // check if the quantity is available in the inventory
+    const result = await orderModel.create(order); // create the order
 
-    const result = await orderModel.create(order);  // create the order
-
-    const newQuantity = product.inventory.quantity - quantity;
+    const newQuantity = product.inventory.quantity - quantity; // it calculate the new quantity
 
     await ProductModel.updateOne(
       { _id: orderID },
-      { $set: { "inventory.quantity": newQuantity } },
+      { $set: { "inventory.quantity": newQuantity } }, // update the quantity in the inventory
     );
 
     if (newQuantity <= 0) {
@@ -28,12 +28,12 @@ const createOrder = async (order: Torder) => {
         { _id: orderID },
         { $set: { "inventory.inStock": false } },
       );
-    }
+    } // if the quantity is less than 0 then it will update the inStock to false
 
-    return result;
+    return result; // return the result
   } else {
     throw new Error("Insufficient quantity available in inventory");
-  }
+  } // if the the requested quantity is not available gives error message
 };
 
 const getOrders = async (email: string) => {
--- src/modules/products/products.Services.ts
+++ src/modules/products/products.Services.ts
@@ -7,27 +7,28 @@ const createProduct = async (data: Tproduct) => {
 };
 
 const getProducts = async (data: string) => {
-  if (data) {                                             // check if the search term is provided 
-    const regex = new RegExp(data, "i");                  // i makes the search case-insensitive 
+  if (data) {
+    // check if the search term is provided
+    const regex = new RegExp(data, "i"); // i makes the search case-insensitive
     const result = await ProductModel.find({
-      $or: [{ name: regex }, { description: regex }, { category: regex }],      // check if the any have the search te
+      $or: [{ name: regex }, { description: regex }, { category: regex }], // check if the any have the search te
     });
-    return result;      // return the result that matches the search term 
+    return result; // return the result that matches the search term
   }
-  const result = await ProductModel.find();   // if no search term is provided, return all the products 
+  const result = await ProductModel.find(); // if no search term is provided, return all the products
   return result;
 };
-const getProduct = async (productId: string) => { 
+const getProduct = async (productId: string) => {
   const result = await ProductModel.findById(productId); // find the product by the id
   return result;
 };
 const updateProduct = async (productId: string, data: object) => {
-  const result = await ProductModel.updateOne({ _id: productId }, data);  // update the product by the id
+  const result = await ProductModel.updateOne({ _id: productId }, data); // update the product by the id
   return result;
 };
 
 const deleteProduct = async (productId: string) => {
-  const result = await ProductModel.deleteOne({ _id: productId });   // delete the product by the id
+  const result = await ProductModel.deleteOne({ _id: productId }); // delete the product by the id
   return result;
 };
 
