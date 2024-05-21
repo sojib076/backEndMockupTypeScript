319942f3a64f8071fe96621006b63c63adb4da2f
sojib076 <sojibds332@gmail.com>
2024-05-21 17:45:13 +0600

    Readme

--- dist/modules/products/products.services.js
+++ dist/modules/products/products.services.js
@@ -16,14 +16,15 @@ const createProduct = (data) => __awaiter(void 0, void 0, void 0, function* () {
     return result;
 });
 const getProducts = (data) => __awaiter(void 0, void 0, void 0, function* () {
-    if (data) { // check if the search term is provided 
-        const regex = new RegExp(data, "i"); // i makes the search case-insensitive 
+    if (data) {
+        // check if the search term is provided
+        const regex = new RegExp(data, "i"); // i makes the search case-insensitive
         const result = yield products_Model_1.ProductModel.find({
             $or: [{ name: regex }, { description: regex }, { category: regex }], // check if the any have the search te
         });
-        return result; // return the result that matches the search term 
+        return result; // return the result that matches the search term
     }
-    const result = yield products_Model_1.ProductModel.find(); // if no search term is provided, return all the products 
+    const result = yield products_Model_1.ProductModel.find(); // if no search term is provided, return all the products
     return result;
 });
 const getProduct = (productId) => __awaiter(void 0, void 0, void 0, function* () {
